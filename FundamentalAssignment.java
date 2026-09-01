0e15b67a360c306943bc7f931fd05fca7afcd5e9
Karlcascolan-code <karlcascolan@gmail.com>
2026-09-01 17:26:28 +0800

    initial commit

---
+++ FundamentalAssignment.java
@@ -0,0 +1,16 @@
+public class FundamentalAssignment {
+    public static void main(String[] args) {
+        String firstName = "Karl Vernon";//Stores the first name of the user. Text value, so String is used.
+        String lastName = "Cascolan";//Stores the last name of the user. Text value, so String is used.
+        String address = "Rosales, Pangasinan";//Stores the address of the user. Text value, so String is used.
+        String mobileNumber = "09934998609";//Stores the mobile number of the user. Text value, so String is used.
+        int age = 18;//Stores the age of the user. Whole number value, so int is used.
+        float gwa = 97.52f;//Stores the GWA of the user. Decimal value, so float is used.
+        System.out.println("First Name: " + firstName);//prints the first name of the user to the console.
+        System.out.println("Last Name: " + lastName);//prints the last name of the user to the console.
+        System.out.println("Address: " + address);//prints the address of the user to the console.
+        System.out.println("Mobile Number: " + mobileNumber);//prints the mobile number of the user to the console.
+        System.out.println("Age: " + age);//prints the age of the user to the console.
+        System.out.println("GWA: " + gwa);//prints the GWA of the user to the console.
+    }
+}
