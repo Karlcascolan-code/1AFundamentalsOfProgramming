a06206cb475d4223d0f216ac3946eff7fe09f3cc
Karlcascolan-code <karlcascolan@gmail.com>
2026-09-22 19:56:50 +0800

    Add 4 Java source files: firstJava.java, secondJava.java, thirdJava.java, fourthJava.java

---
+++ src/firstJava.java
@@ -0,0 +1,7 @@
+public class firstJava {
+    public static void main(String[] args){
+
+        System.out.println("Karl Vernon Calpo Cascolan");
+        System.out.println("BSIT - 1A");
+    }
+}
---
+++ src/secondJava.java
@@ -0,0 +1,16 @@
+public class secondJava {
+    public static void main (String[] args){
+
+        String name = "Karl Vernon C. Cascolan";
+        String address = "Rosales, Pangasinan";
+        String contact = "09934998609";
+        int age = 18;
+        String crush = "Rojel L. Bartolome";
+
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(contact);
+        System.out.println(age);
+        System.out.println(crush);
+    }
+}
